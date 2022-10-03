8e7abf167e00f5bea618e02f19d3950b33ac34fb
CodenameSushi <fhenrique08@gmail.com>
2022-10-03 20:51:57 -0300

    Exercicios 027

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 const frutas = ["laranja", "limão", "uva"];
+
+let listaFrutas = document.getElementsByTagName("li")
+
+let fruta = document.getElementById("fruta");
+
+for (let tamanhaArray in frutas ) {
+    listaFrutas[tamanhaArray].innerHTML = frutas[tamanhaArray]
+}
+
+// document.getElementById("fruta-1").innerHTML = frutas[0];
+// document.getElementById("fruta-2").innerHTML = frutas[1];
+// document.getElementById("fruta-3").innerHTML = frutas[2];
+
+// function imprimeFruta() {
+//     console.log(fruta.value)
+//     document.getElementById("fruta-4").innerHTML = fruta.value
+//     document.getElementById("fruta").value = "";
+
+// }
+
+function imprimeFruta() {
+    console.log(fruta.value)
+    const lista = document.getElementById("lista-de-frutas")
+    lista.innerHTML += `<li>${fruta.value}</li>`
+    fruta.value = ""
+   }
+
+// document.getElementById("fruta-4").innerHTML = fruta.value
+// document.getElementById("fruta").value = "";
